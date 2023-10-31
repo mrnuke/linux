2865f9709d3a2e30eb2e374f800aa2eb63ca7414
Varadarajan Narayanan <quic_varada@quicinc.com>
2023-10-31 12:41:39 +0530

    cpufreq: qcom-nvmem: Introduce cpufreq for ipq95xx
    IPQ95xx SoCs have different OPPs available for the CPU based on
    the SoC variant. This can be determined from an eFuse register
    present in the silicon.
    
    Added support for ipq95xx on nvmem driver which helps to
    determine OPPs at runtime based on the eFuse register which
    has the CPU frequency limits. opp-supported-hw dt binding
    can be used to indicate the available OPPs for each limit.
    
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Praveenkumar I <ipkumar@codeaurora.org>
    Signed-off-by: Kathiravan T <quic_kathirav@quicinc.com>
    Signed-off-by: Varadarajan Narayanan <quic_varada@quicinc.com>
    [ Viresh: Fixed subject ]
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -179,6 +179,7 @@ static const struct of_device_id blocklist[] __initconst = {
 
 	{ .compatible = "qcom,ipq5332", },
 	{ .compatible = "qcom,ipq8064", },
+	{ .compatible = "qcom,ipq9574", },
 	{ .compatible = "qcom,apq8064", },
 	{ .compatible = "qcom,msm8974", },
 	{ .compatible = "qcom,msm8960", },
