edffb3790dbb0d2a8194c6371e7112baa770cd5c
Luo Jie <quic_luoj@quicinc.com>
2023-12-27 13:13:46 +0800

    net: ethernet: qualcomm: Initialize PPE queue settings
    Configure unicast and multicast hardware queues to forward
    the traffic between PPE ports.
    
    Each PPE port is assigned with the specific queue resource,
    and the egress queue ID is decided by the priority and the
    RSS hash value of packet.
    
    Change-Id: I3e4d4e12548a12b11f129106678375cc3b58828d
    Signed-off-by: Luo Jie <quic_luoj@quicinc.com>

--- drivers/net/ethernet/qualcomm/ppe/ppe_config.h
+++ drivers/net/ethernet/qualcomm/ppe/ppe_config.h
@@ -6,6 +6,13 @@
 #ifndef __PPE_CONFIG_H__
 #define __PPE_CONFIG_H__
 
+/* There are different queue config ranges for the destination port,
+ * CPU code and service code.
+ */
+#define PPE_QUEUE_BASE_DEST_PORT		0
+#define PPE_QUEUE_BASE_CPU_CODE			1024
+#define PPE_QUEUE_BASE_SERVICE_CODE		2048
+
 /**
  * struct ppe_qos_scheduler_cfg - PPE QoS scheduler configuration.
  * @flow_id: PPE flow ID.
@@ -26,6 +33,26 @@ struct ppe_qos_scheduler_cfg {
 	int node_frame_mode;
 };
 
+/**
+ * struct ppe_queue_ucast_dest - PPE unicast queue destination.
+ * @src_profile: Source profile.
+ * @service_code_en: Enable service code.
+ * @service_code: Service code.
+ * @cpu_code_en: Enable CPU code.
+ * @cpu_code: CPU code.
+ * @dest_port: destination port.
+ *
+ * PPE egress queue ID is decided by the egress port ID.
+ */
+struct ppe_queue_ucast_dest {
+	int src_profile;
+	bool service_code_en;
+	int service_code;
+	bool cpu_code_en;
+	int cpu_code;
+	int dest_port;
+};
+
 int ppe_hw_config(struct ppe_device *ppe_dev);
 int ppe_queue_scheduler_set(struct ppe_device *ppe_dev,
 			    int node_id, bool flow_level, int port,
@@ -33,4 +60,18 @@ int ppe_queue_scheduler_set(struct ppe_device *ppe_dev,
 int ppe_queue_scheduler_get(struct ppe_device *ppe_dev,
 			    int node_id, bool flow_level, int *port,
 			    struct ppe_qos_scheduler_cfg *scheduler_cfg);
+int ppe_queue_ucast_base_set(struct ppe_device *ppe_dev,
+			     struct ppe_queue_ucast_dest queue_dst,
+			     int queue_base,
+			     int profile_id);
+int ppe_queue_ucast_pri_class_set(struct ppe_device *ppe_dev,
+				  int profile_id,
+				  int priority,
+				  int class_offset);
+int ppe_queue_ucast_hash_class_set(struct ppe_device *ppe_dev,
+				   int profile_id,
+				   int rss_hash,
+				   int class_offset);
+int ppe_port_resource_get(struct ppe_device *ppe_dev, int port, int type,
+			  int *res_start, int *res_end);
 #endif
