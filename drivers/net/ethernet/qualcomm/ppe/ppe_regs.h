1af5a5414fc7193a37ba443d556c9ef4d3e5a638
Luo Jie <quic_luoj@quicinc.com>
2024-01-10 19:40:17 +0800

    net: ethernet: qualcomm: Add PPE queue management config
    QM(queue management) config decides the length of queue and the
    threshold to drop packet, there are two kinds of queue, unicast
    queue and multicast queue to transmit different type of packet.
    
    Signed-off-by: Luo Jie <quic_luoj@quicinc.com>

--- drivers/net/ethernet/qualcomm/ppe/ppe_regs.h
+++ drivers/net/ethernet/qualcomm/ppe/ppe_regs.h
@@ -7,6 +7,14 @@
 #ifndef __PPE_REGS_H__
 #define __PPE_REGS_H__
 
+#define PPE_EG_BRIDGE_CONFIG					0x20044
+#define PPE_EG_BRIDGE_CONFIG_QUEUE_CNT_EN			BIT(2)
+
+#define PPE_DEQ_OPR_TBL						0x430000
+#define PPE_DEQ_OPR_TBL_NUM					300
+#define PPE_DEQ_OPR_TBL_INC					0x10
+#define PPE_ENQ_OPR_TBL_DEQ_DISABLE				BIT(0)
+
 #define PPE_BM_PORT_FC_MODE					0x600100
 #define PPE_BM_PORT_FC_MODE_NUM					15
 #define PPE_BM_PORT_FC_MODE_INC					4
@@ -45,5 +53,103 @@ union ppe_bm_port_fc_cfg_u {
 	u32 val[2];
 	struct ppe_bm_port_fc_cfg bf;
 };
+
+#define PPE_AC_UNI_QUEUE_CFG_TBL				0x848000
+#define PPE_AC_UNI_QUEUE_CFG_TBL_NUM				256
+#define PPE_AC_UNI_QUEUE_CFG_TBL_INC				0x10
+
+/* PPE unicast queue(0-255) configurations, the threshold supports to be
+ * configured static or dynamic.
+ *
+ * For dynamic threshold, the queue threshold depends on the remain buffer.
+ */
+struct ppe_ac_uni_queue_cfg {
+	u32 ac_en:1,
+	    wred_en:1,
+	    force_ac_en:1,
+	    color_aware:1,
+	    ac_grp_id:2,
+	    prealloc_limit:11,
+	    shared_dynamic:1,
+	    shared_weight:3,
+	    shared_ceiling:11;
+	u32 gap_grn_grn_min:11,
+	    gap_grn_yel_max:11,
+	    gap_grn_yel_min_0:10;
+	u32 gap_grn_yel_min_1:1,
+	    gap_grn_red_max:11,
+	    gap_grn_red_min:11,
+	    red_resume_0:9;
+	u32 red_resume_1:2,
+	    yel_resume:11,
+	    grn_resume:11,
+	    res0:8;
+};
+
+union ppe_ac_uni_queue_cfg_u {
+	u32 val[4];
+	struct ppe_ac_uni_queue_cfg bf;
+};
+
+#define PPE_AC_MUL_QUEUE_CFG_TBL				0x84a000
+#define PPE_AC_MUL_QUEUE_CFG_TBL_NUM				44
+#define PPE_AC_MUL_QUEUE_CFG_TBL_INC				0x10
+
+/* PPE multicast queue(256-299) configurations, the mutlicast queues are
+ * fixed to the PPE ports, which only support static threshold.
+ */
+struct ppe_ac_mul_queue_cfg {
+	u32 ac_en:1,
+	    force_ac_en:1,
+	    color_aware:1,
+	    ac_grp_id:2,
+	    prealloc_limit:11,
+	    shared_ceiling:11,
+	    gap_grn_yel_0:5;
+	u32 gap_grn_yel_1:6,
+	    gap_grn_red:11,
+	    red_resume:11,
+	    yel_resume_0:4;
+	u32 yel_resume_1:7,
+	    grn_resume:11,
+	    res0:14;
+};
+
+union ppe_ac_mul_queue_cfg_u {
+	u32 val[3];
+	struct ppe_ac_mul_queue_cfg bf;
+};
+
+#define PPE_AC_GRP_CFG_TBL					0x84c000
+#define PPE_AC_GRP_CFG_TBL_NUM					4
+#define PPE_AC_GRP_CFG_TBL_INC					0x10
+
+/* PPE admission control of group configurations */
+struct ppe_ac_grp_cfg {
+	u32 ac_en:1,
+	    force_ac_en:1,
+	    color_aware:1,
+	    gap_grn_red:11,
+	    gap_grn_yel:11,
+	    dp_thrd_0:7;
+	u32 dp_thrd_1:4,
+	    limit:11,
+	    red_resume:11,
+	    yel_resume_0:6;
+	u32 yel_resume_1:5,
+	    grn_resume:11,
+	    prealloc_limit:11,
+	    res0:5;
+};
+
+union ppe_ac_grp_cfg_u {
+	u32 val[3];
+	struct ppe_ac_grp_cfg bf;
+};
+
+#define PPE_ENQ_OPR_TBL						0x85c000
+#define PPE_ENQ_OPR_TBL_NUM					300
+#define PPE_ENQ_OPR_TBL_INC					0x10
+#define PPE_ENQ_OPR_TBL_ENQ_DISABLE				BIT(0)
 
 #endif
