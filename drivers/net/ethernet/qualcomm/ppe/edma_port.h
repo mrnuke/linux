a29e3cdfe8fc46a62acfd62bba638fec80f7d6e9
Suruchi Agarwal <quic_suruchia@quicinc.com>
2024-03-21 16:21:19 -0700

    net: ethernet: qualcomm: Add Rx Ethernet DMA support
    Add Rx queues, rings, descriptors configurations and
    DMA support for the EDMA.
    
    Change-Id: I612bcd661e74d5bf3ecb33de10fd5298d18ff7e9
    Co-developed-by: Pavithra R <quic_pavir@quicinc.com>
    Signed-off-by: Pavithra R <quic_pavir@quicinc.com>
    Signed-off-by: Suruchi Agarwal <quic_suruchia@quicinc.com>
    Alex G: add missing functions that were previously in ppe_api.c:
            - ppe_edma_queue_resource_get()
            - ppe_edma_ring_to_queues_config()
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- drivers/net/ethernet/qualcomm/ppe/edma_port.h
+++ drivers/net/ethernet/qualcomm/ppe/edma_port.h
@@ -15,14 +15,45 @@
 					| NETIF_F_TSO6)
 
 /**
+ * struct edma_port_rx_stats - EDMA RX per CPU stats for the port.
+ * @rx_pkts: Number of Rx packets
+ * @rx_bytes: Number of Rx bytes
+ * @rx_drops: Number of Rx drops
+ * @rx_nr_frag_pkts: Number of Rx nr_frags packets
+ * @rx_fraglist_pkts: Number of Rx fraglist packets
+ * @rx_nr_frag_headroom_err: nr_frags headroom error packets
+ * @syncp: Synchronization pointer
+ */
+struct edma_port_rx_stats {
+	u64 rx_pkts;
+	u64 rx_bytes;
+	u64 rx_drops;
+	u64 rx_nr_frag_pkts;
+	u64 rx_fraglist_pkts;
+	u64 rx_nr_frag_headroom_err;
+	struct u64_stats_sync syncp;
+};
+
+/**
+ * struct edma_port_pcpu_stats - EDMA per cpu stats data structure for the port.
+ * @rx_stats: Per CPU Rx statistics
+ */
+struct edma_port_pcpu_stats {
+	struct edma_port_rx_stats __percpu *rx_stats;
+};
+
+/**
  * struct edma_port_priv - EDMA port priv structure.
  * @ppe_port: Pointer to PPE port
  * @netdev: Corresponding netdevice
+ * @pcpu_stats: Per CPU netdev statistics
+ * @txr_map: Tx ring per-core mapping
  * @flags: Feature flags
  */
 struct edma_port_priv {
 	struct ppe_port *ppe_port;
 	struct net_device *netdev;
+	struct edma_port_pcpu_stats pcpu_stats;
 	unsigned long flags;
 };
 
