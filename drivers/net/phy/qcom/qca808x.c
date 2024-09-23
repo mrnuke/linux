8d86adc755c2a815c041fa51f10ccde02714af78
Luo Jie <quic_luoj@quicinc.com>
2024-09-23 20:59:40 +0800

    net: phy: qca808x: Add QCA8084 SerDes init function
    When QCA8084 works on 10G-QXGMII, the XPCS and PCS need to be
    configured in the PHY package init function.
    
    Change-Id: Iac48c44f0e80adf055fa9c2095e99a04ba24c4bb
    Signed-off-by: Luo Jie <quic_luoj@quicinc.com>

--- drivers/net/phy/qcom/qca808x.c
+++ drivers/net/phy/qcom/qca808x.c
@@ -947,6 +947,14 @@ static int qca8084_phy_package_config_init_once(struct phy_device *phydev)
 
 	usleep_range(10000, 11000);
 
+	/* Configure PCS working on 10G-QXGMII mode */
+	if (phydev->interface == PHY_INTERFACE_MODE_10G_QXGMII) {
+		ret = qca8084_qxgmii_set_mode(shared_priv->mdiodev[1],
+					      shared_priv->mdiodev[0]);
+		if (ret)
+			return ret;
+	}
+
 	/* Initialize the PHY package clock and reset, which is the
 	 * necessary config sequence after GPIO reset on the PHY package.
 	 */
@@ -1185,6 +1193,9 @@ static int qca8084_probe(struct phy_device *phydev)
 
 static void qca8084_remove(struct phy_device *phydev)
 {
+	if (phydev->interface != PHY_INTERFACE_MODE_10G_QXGMII)
+		return;
+
 	if (phy_package_remove_once(phydev))
 		qca8084_phy_package_remove_once(phydev);
 }
