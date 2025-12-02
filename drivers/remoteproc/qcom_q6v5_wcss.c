10731196d241dd15ff52c16670a3be68c8d1d7f3
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2025-12-01 19:12:08 -0600

    [HACK] remoteproc: qcom_q6v5_wcss: print Q6 version
    I suspect these will be useless upstream, and they also require a
    "reg" entry in the wcss node. But let's print them for now to make
    sure that the remoteproc/q6 is working.

--- drivers/remoteproc/qcom_q6v5_wcss.c
+++ drivers/remoteproc/qcom_q6v5_wcss.c
@@ -237,6 +237,7 @@ static int q6v5_wcss_reset(struct q6v5_wcss *wcss)
 static int q6v5_wcss_start(struct rproc *rproc)
 {
 	struct q6v5_wcss *wcss = rproc->priv;
+	uint32_t val;
 	int ret;
 
 	qcom_q6v5_prepare(&wcss->q6v5);
@@ -278,6 +279,10 @@ static int q6v5_wcss_start(struct rproc *rproc)
 	ret = qcom_q6v5_wait_for_start(&wcss->q6v5, 5 * HZ);
 	if (ret == -ETIMEDOUT)
 		dev_err(wcss->dev, "start timed out\n");
+
+	/* Read the version registers to make sure WCSS is out of reset */
+	val = readl(wcss->reg_base);
+	dev_info(wcss->dev, "QDSP6SS Version : 0x%x\n", val);
 
 	return ret;
 
